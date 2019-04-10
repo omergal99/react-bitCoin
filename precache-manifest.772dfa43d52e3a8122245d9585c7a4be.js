e430361867d211b864dfeeac9727ed6ded89598e
omergal99 <omergal99gm@gmail.com>
2019-04-10 17:05:55 +0300

    Updates

---
+++ precache-manifest.772dfa43d52e3a8122245d9585c7a4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c7cd4aa705669c58d60",
+    "url": "/react-bitCoin/static/js/runtime~main.5a36ab58.js"
+  },
+  {
+    "revision": "e517597d106d8c48ee29",
+    "url": "/react-bitCoin/static/js/main.0b8e35bd.chunk.js"
+  },
+  {
+    "revision": "f894f9a6e28b0002dcd0",
+    "url": "/react-bitCoin/static/js/2.59d32b49.chunk.js"
+  },
+  {
+    "revision": "e517597d106d8c48ee29",
+    "url": "/react-bitCoin/static/css/main.6139a53d.chunk.css"
+  },
+  {
+    "revision": "81e2f1dcfb8e07114a5c8073099a59df",
+    "url": "/react-bitCoin/index.html"
+  }
+];
